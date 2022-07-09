c130b77bcf36d03543875a519e47b49220c322d5
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-09 09:49:29 +0530

    Added : Player choose valid cels to move

--- com/bridzelab/TicTocToe.java
+++ com/bridzelab/TicTocToe.java
@@ -1,5 +1,6 @@
 package com.bridzelab;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTocToe {
@@ -7,16 +8,17 @@ public class TicTocToe {
     static Scanner scanner = new Scanner(System.in);
     static char user = '1';
     static int userPosition;
+    static int computerPosition;
+    static char computerTurn;
     static char userTurn;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Toc toe Program");
         ticTacToe();
         createBoard();
-        user();
-        moveLocation();
-
+        userChoose();
     }
+
 
     public static void createBoard() {
         System.out.println(gameBoard[0] + " | " + gameBoard[1] + " | " + gameBoard[2]);
@@ -24,7 +26,7 @@ public class TicTocToe {
         System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
     }
 
-    public static char user() {
+    public static char userChoose() {
         System.out.println("Enter your choice  X or O only");
         return scanner.next().toUpperCase().charAt(0);
     }
@@ -90,6 +92,83 @@ public class TicTocToe {
                     System.out.println("Choose correct number");
 
             }
+        }
+    }
+
+
+    public static void computerMove() {
+        boolean flag = false;
+        while (true) {
+            computerPosition= (int) (Math.random() * 8);
+            switch (computerPosition) {
+                case 0:
+                    if (gameBoard[0] == ' ') {
+                        gameBoard[0] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+                case 1:
+                    if (gameBoard[1] == ' ') {
+                        gameBoard[1] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+                case 2:
+                    if (gameBoard[2] == ' ') {
+                        gameBoard[2] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+                case 3:
+                    if (gameBoard[3] == ' ') {
+                        gameBoard[3] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+                case 4:
+                    if (gameBoard[4] == ' ') {
+                        gameBoard[4] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+                case 5:
+                    if (gameBoard[5] == ' ') {
+                        gameBoard[5] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+                case 6:
+                    if (gameBoard[6] == ' ') {
+                        gameBoard[6] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+                case 7:
+                    if (gameBoard[7] == ' ') {
+                        gameBoard[7] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+                case 8:
+                    if (gameBoard[8] == ' ') {
+                        gameBoard[8] = computerTurn;
+                        flag = true;
+                    }
+                    break;
+
+            }
+            if (flag) {
+                break;
+            }
+
         }
     }
 
