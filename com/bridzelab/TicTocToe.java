c68f116e7685fcb18321f3c06af65c40e3e784c6
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-09 08:56:03 +0530

    Added : player choose valid cells

--- com/bridzelab/TicTocToe.java
+++ com/bridzelab/TicTocToe.java
@@ -8,6 +8,7 @@ public class TicTocToe {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Toc toe Program");
+        ticTacToe();
         createBoard();
         user();
 
@@ -22,5 +23,11 @@ public class TicTocToe {
     public static char user() {
         System.out.println("Enter your choice  X or O only");
         return scanner.next().toUpperCase().charAt(0);
+    }
+
+    public static void ticTacToe() {
+        for (int i = 0; i < gameBoard.length; i++) {
+            gameBoard[i] = ' ';
+        }
     }
 }
