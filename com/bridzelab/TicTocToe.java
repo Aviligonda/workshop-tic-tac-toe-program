7b1fe90064ef29e185e5058916ac01c843915c5e
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-09 08:51:42 +0530

    Added : player choose X or O

--- com/bridzelab/TicTocToe.java
+++ com/bridzelab/TicTocToe.java
@@ -1,11 +1,15 @@
 package com.bridzelab;
 
+import java.util.Scanner;
+
 public class TicTocToe {
     static char[] gameBoard = new char[10];
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Toc toe Program");
         createBoard();
+        user();
 
     }
 
@@ -15,4 +19,8 @@ public class TicTocToe {
         System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
     }
 
+    public static char user() {
+        System.out.println("Enter your choice  X or O only");
+        return scanner.next().toUpperCase().charAt(0);
+    }
 }
